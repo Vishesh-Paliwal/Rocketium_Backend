61e7aa10d31514573aac16a20e008e50ee3f3a5b
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-07-30 23:50:00 +0530

    add basic sorting and filtering

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,14 +2,30 @@ const { getData } = require('../models/dataModel');
 
 function getDataController(req, res) {
   let data = getData();
-  const { sort, filter } = req.query;
+  const { sortField, sortOrder = 'asc', filterfield ,filter } = req.query;
 
-  if (filter) {
-    data = data.filter(item => item.someField === filter);
+  if (filterfield==="id") {
+    data = data.filter(item => item.id === filter);
+  }
+  if (filterfield==="name") {
+    data = data.filter(item => item.name === filter);
+  }
+  if (filterfield==="language") {
+    data = data.filter(item => item.language === filter);
+  }
+  if (filterfield==="bio") {
+     data = data.filter(item => item.bio === filter);
+  }
+  if (filterfield==="version") {
+     data = data.filter(item => item.version === filter);
   }
 
-  if (sort) {
-    data.sort((a, b) => (a.someField > b.someField ? 1 : -1));
+  if (sortField) {
+    data.sort((a, b) => {
+      if (a[sortField] < b[sortField]) return sortOrder === 'asc' ? -1 : 1;
+      if (a[sortField] > b[sortField]) return sortOrder === 'asc' ? 1 : -1;
+      return 0;
+    });
   }
 
   res.json(data);
